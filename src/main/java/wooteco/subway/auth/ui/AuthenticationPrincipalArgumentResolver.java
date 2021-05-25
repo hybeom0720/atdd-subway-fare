4a51b082e3cd1ae5910409a900aebecf08e43e57
YeongBeom Heo <hybeom@gmail.com>
2021-05-25 14:59:16 +0900

    feat : for middleBear

--- src/main/java/wooteco/subway/auth/ui/AuthenticationPrincipalArgumentResolver.java
+++ src/main/java/wooteco/subway/auth/ui/AuthenticationPrincipalArgumentResolver.java
@@ -1,23 +1,15 @@
 package wooteco.subway.auth.ui;
 
 import org.springframework.core.MethodParameter;
-import org.springframework.web.bind.support.WebDataBinderFactory;
-import org.springframework.web.context.request.NativeWebRequest;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.method.support.ModelAndViewContainer;
 import wooteco.subway.auth.application.AuthService;
 import wooteco.subway.auth.application.AuthorizationException;
 import wooteco.subway.auth.domain.AuthenticationPrincipal;
-import wooteco.subway.auth.infrastructure.AuthorizationExtractor;
 import wooteco.subway.member.domain.LoginMember;
 
-import javax.servlet.http.HttpServletRequest;
-
-public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArgumentResolver {
-    private AuthService authService;
+public class AuthenticationPrincipalArgumentResolver extends AuthenticationArgumentResolver {
 
     public AuthenticationPrincipalArgumentResolver(AuthService authService) {
-        this.authService = authService;
+        super(authService);
     }
 
     @Override
@@ -26,9 +18,7 @@ public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArg
     }
 
     @Override
-    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
-        String credentials = AuthorizationExtractor.extract(webRequest.getNativeRequest(HttpServletRequest.class));
-        LoginMember member = authService.findMemberByToken(credentials);
+    public LoginMember validMember(LoginMember member) {
         if (member.getId() == null) {
             throw new AuthorizationException();
         }
